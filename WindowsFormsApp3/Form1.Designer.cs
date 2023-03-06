5d55fc8aa898b62a210881aea34b59c35a32e88b
ilinMFP <ilinMFP@ILINMFP-LAPTOP>
2023-03-06 10:06:31 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(442, 192);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать заново";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.label1.Location = new System.Drawing.Point(17, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ваш вариант";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.25F);
+            this.label2.Location = new System.Drawing.Point(27, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(436, 53);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Игра \"Угадай число\":\r\n Компьютер загадывает число от 0 до 100, а у пользователя е" +
+    "сть 7 попыток для угадывания числа";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.button2.Location = new System.Drawing.Point(208, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Предложить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.label3.Location = new System.Drawing.Point(27, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Число меньше загаданного";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.label4.Location = new System.Drawing.Point(28, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Число больше загаданного";
+            this.label4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.button3.Location = new System.Drawing.Point(51, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 87);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Начать игру";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Оставшиеся ходы: n";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 386);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        int randNum, userNum, prevNum, hodCount;
+        bool gameFinished;
+        const int maxCount = 7;
+        public Form1()
+        {
+            InitializeComponent();
+            button3.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userNum = int.Parse(textBox1.Text);
+            if (userNum == prevNum) 
+            {
+                return;
+            }
+            prevNum = userNum;
+            label3.Visible = false;
+            label4.Visible = false;
+            hodCount++;
+            label5.Text = "Оставшиеся ходы : " + (maxCount - hodCount).ToString();
+            if (userNum == randNum)
+            {
+                MessageBox.Show("Вы угадали число за " + hodCount.ToString() + " ходов.");
+                FinishGame();
+            }
+
+            else if (userNum < randNum)
+            {
+                label4.Visible = true;
+            }
+
+            else if (userNum > randNum)
+            {
+                label3.Visible = true;
+            }
+
+            if ((hodCount == maxCount) &&(!gameFinished))
+            {
+                MessageBox.Show("Вам не удалось угадать число за " + maxCount.ToString() + " ходов. Игра окончена.");
+                FinishGame();
+            }
+        }
+        private void SetupGame()
+        {
+            gameFinished = false;
+            Random r = new Random();
+            randNum = r.Next(0, 101);
+            hodCount = 0;
+            textBox1.Text = "";
+            prevNum = 0;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            button1.Visible = false;
+            button2.Visible = true;
+            textBox1.Visible = true;
+            label1.Visible = true;
+            SetupGame();
+            label5.Visible = true;
+            label5.Text = "Оставшиеся ходы : " + maxCount.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = true;
+            textBox1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            SetupGame();
+            label5.Visible = true;
+            label5.Text = "Оставшиеся ходы : " + maxCount.ToString();
+        }
+
+        private void FinishGame()
+        {
+            button1.Visible = true;
+            button2.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            textBox1.Visible = false;
+            gameFinished = true;
+        }
+    }
+}
